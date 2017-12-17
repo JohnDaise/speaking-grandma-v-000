8c3118a306f2d7b81406f6672be43ed896524402
John Daise <jdaise@gmail.com>
2017-12-17 02:44:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 def speak_to_grandma(phrase)
 
-  if phrase.upcase == true
+  if phrase.upcase = true
 
   return "NO, NOT SINCE 1938!"
 
@@ -26,5 +26,5 @@ def speak_to_grandma(phrase)
 
   return "HUH?! SPEAK UP, SONNY!"
 
-end 
+end
 end
