e36dd681b20bf4a36959e13641276924e05b9357
John Daise <jdaise@gmail.com>
2017-12-17 02:43:53 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,25 +14,16 @@
 
 def speak_to_grandma(phrase)
 
-if phrase.upcase == true
+  if phrase.upcase == true
 
   return "NO, NOT SINCE 1938!"
 
-elsif phrase == "I LOVE YOU GRANDMA!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
 
   return "I LOVE YOU TOO PUMPKIN!"
 
+  else
 
-else 
-  
   return HUH?! SPEAK UP, SONNY!"
-  
 
-#elsif
-
-#  
-
-
-end
-
-end
+end 
